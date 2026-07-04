7168415b763d89ba6e1070984774eaa29a468af4
Lorenzo Neri <nerilorenzo412@gmail.com>
2026-07-04 23:59:19 +0200

    New exercise: Loop

--- Level 3/Ex02.js
+++ Level 3/Ex02.js
@@ -1,5 +1,11 @@
+//Loop through an array and print each item to the console
 let inventory = ["Sword", "Shield", "Potion", "Bow"];
 
 for(let i = 0; i < inventory.length; i++) {
     console.log(inventory[i]);
+}
+
+//bonus
+for(let i = 0; i < inventory.length; i++) {
+    console.log(i + " " + inventory[i]);
 }
