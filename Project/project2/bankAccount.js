1fa516714eff21a9bac3630ef892259d1d645768
Lorenzo Neri <nerilorenzo412@gmail.com>
2026-07-20 01:02:07 +0200

    new project: bankAccount

---
+++ Project/project2/bankAccount.js
@@ -0,0 +1,46 @@
+bankAccount = {
+    owner: "Lollo",
+    balance: 1000,
+    transactions: []
+}
+
+function deposit(amount) {
+    bankAccount.balance += amount;
+    bankAccount.transactions.push({ type: "deposit", amount: amount });
+    console.log(`Deposited ${amount}. New balance: ${bankAccount.balance}`);
+}
+
+function withdraw(amount) {
+    if (bankAccount.balance >= amount) {
+        bankAccount.balance -= amount;
+        bankAccount.transactions.push({ type: "withdrawal", amount: amount });
+        console.log(`Withdrew ${amount}. New balance: ${bankAccount.balance}`);
+    }else{
+        console.log(`Insufficient funds. Current balance: ${bankAccount.balance}`);
+    }
+}
+
+function displayTransactions() {
+    for (let i = 0; i < bankAccount.transactions.leight; i++) {
+        transaction = bankAccount.transactions[i];
+        console.log(`Transaction: ${transaction.type}, Amount: ${transaction.amount}`)
+    }
+}
+
+function displayBalance() {
+    console.log(`Current balance: ${bankAccount.balance}`);
+}
+
+displayBalance()
+
+deposit(200)
+
+withdraw(150)
+
+withdraw(800)
+
+deposit(50)
+
+displayBalance()
+
+displayTransactions()
